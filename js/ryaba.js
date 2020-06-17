5f709af77a1007e8063f65a06140e856e24f24f1
Gadya <zakuson2001@inbox.ru>
2020-06-18 01:06:49 +0300

    All done! TODO: make it beautiful

--- js/ryaba.js
+++ js/ryaba.js
@@ -1,6 +1,15 @@
-const fairyTale = $('span.story-text').text();
+const objText = {"text":["Жили-были {var1} да {var2}","Была у них {var3}","Снесла {var3} {var4}, не простое - золотое","- {var1} бил, бил - не разбил","- {var2} била, била - не разбила","{var5} бежала, {var6} задела, {var4} упало и разбилось.","{var1} плачет, {var2} плачет, а {var3} кудахчет:","{speach}"]};
+const fairyTaleWithVars = objText.text;
 
-function getInputs() {
+function formatStr(notFormatFT){ //Функция добавления переносов строк
+  let formatFT='';
+  notFormatFT.forEach(element => {
+    formatFT += element+"</br>";
+  });
+  return formatFT;
+}
+
+function getInputs() { // Функция получения массива значений инпутов
   const var1 = $('#var1').val();
   const var2 = $('#var2').val();
   const var3 = $('#var3').val();
@@ -8,30 +17,26 @@ function getInputs() {
   const var5 = $('#var5').val();
   const var6 = $('#var6').val();
   const speach= $('#speach').val();
-  arrOfInputs = [var1,var2,var3,var4,var5,var6,speach];
+  let arrOfInputs = {"var1": var1, "var2": var2, "var3": var3, "var4": var4, "var5": var5, "var6": var6 ,"speach": speach};
   return arrOfInputs;
 }
-function makeFairyTale(arr, text){
-  arr.forEach(element => text.replace('{var1}', element));
-}
+
+$(".btn-create-text").click(function(e){
+  e.preventDefault();
+  $("#result-text").html(formatStr(fairyTaleWithVars));
+});
 
 $(document).ready(function() {
   $(".btn-change-var").click(function(e){
     e.preventDefault();
     arr=getInputs();
-    ft = makeFairyTale(); 
-    $("#result-text").text(ft);
+    rez=formatStr(fairyTaleWithVars);
+    for (key in arr){
+      rez = rez.replace(new RegExp(key,'g'), arr[key]); //Заменяем ключ на его значение
+    }
+    rez = rez.replace(new RegExp("{",'g'), ''); //Убираем оставшиеся скобки
+    rez = rez.replace(new RegExp("}",'g'), ''); //Убираем оставшиеся скобки
+    $("#result-text").html(rez); // Отдаем результат на страницу
   });
 });
 
-
-//   // надо сделать так чтобы сообщения подменились на значения из формы
-//   let text = "Здесь могла быть ваша реклама";
-// 	$("#result").text(text);
-// }
-
-// function init() {
-// 	$("#button-fetch").click(handleButton);
-// }
-
-// $(document).ready(init);
